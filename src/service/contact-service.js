1517cdcd3c02861e2e6cb0f78173423827ce26ba
chuluq <chuluq@arvis.id>
2025-02-24 21:35:30 +0700

    feat: add search contact api

--- src/service/contact-service.js
+++ src/service/contact-service.js
@@ -1,7 +1,7 @@
 import {validate} from '../validation/validation.js';
 import {
   createContactValidation,
-  getContactValidation, updateContactValidation,
+  getContactValidation, searchContactValidation, updateContactValidation,
 } from '../validation/contact-validation.js';
 import {prismaClient} from '../application/database.js';
 import {ResponseError} from '../error/response-error.js';
@@ -90,15 +90,85 @@ const remove = async (user, contactId) => {
     },
   });
 
-  if(totalInDatabase !== 1) {
+  if (totalInDatabase !== 1) {
     throw new ResponseError(404, 'contact is not found');
   }
 
   return prismaClient.contact.delete({
     where: {
       id: contactId,
-    }
-  })
+    },
+  });
+};
+
+const search = async (user, request) => {
+  request = validate(searchContactValidation, request);
+
+  // 1 ((page - 1) * size) = 0
+  // 2 ((page - 1) * size) = 10
+  const skip = (request.page - 1) * request.size;
+
+  const filters = [];
+
+  filters.push({
+    username: user.username,
+  });
+
+  if (request.name) {
+    filters.push({
+      OR: [
+        {
+          first_name: {
+            contains: request.name,
+          },
+        },
+        {
+          last_name: {
+            contains: request.name,
+          },
+        },
+      ],
+    });
+  }
+  if (request.email) {
+    filters.push(
+        {
+          email: {
+            contains: request.email,
+          },
+        });
+  }
+  if (request.phone) {
+    filters.push(
+        {
+          phone: {
+            contains: request.phone,
+          },
+        });
+  }
+
+  const contacts = await prismaClient.contact.findMany({
+    where: {
+      AND: filters,
+    },
+    take: request.size,
+    skip: skip,
+  });
+
+  const totalItems = await prismaClient.contact.count({
+    where: {
+      AND: filters,
+    },
+  });
+
+  return {
+    data: contacts,
+    paging: {
+      page: request.page,
+      total_item: totalItems,
+      total_page: Math.ceil(totalItems / request.size),
+    },
+  };
 };
 
 export default {
@@ -106,4 +176,5 @@ export default {
   get,
   update,
   remove,
+  search,
 };
--- test/contact.test.js
+++ test/contact.test.js
@@ -1,6 +1,8 @@
 import {
+  createManyTestContacts,
   createTestContact,
-  createTestUser, getTestContact,
+  createTestUser,
+  getTestContact,
   removeAllTestContacts,
   removeTestUser,
 } from './test-util.js';
@@ -181,8 +183,84 @@ describe('DELETE /api/contacts/:contactId', () => {
   it('should reject if contact is not found', async () => {
     let testContact = await getTestContact();
     const result = await supertest(web).
-        delete('/api/contacts/' + testContact.id + 1).set('Authorization', 'test');
+        delete('/api/contacts/' + testContact.id + 1).
+        set('Authorization', 'test');
 
     expect(result.status).toBe(404);
+  });
+});
+
+describe('GET /api/contacts', () => {
+  beforeEach(async () => {
+    await createTestUser();
+    await createManyTestContacts();
+  });
+
+  afterEach(async () => {
+    await removeAllTestContacts();
+    await removeTestUser();
+  });
+
+  it('should search without parameter', async () => {
+    const result = await supertest(web).
+        get('/api/contacts').
+        set('Authorization', 'test');
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(10);
+    expect(result.body.paging.page).toBe(1);
+    expect(result.body.paging.total_page).toBe(2);
+    expect(result.body.paging.total_item).toBe(15);
+  });
+
+  it('should search to page 2', async () => {
+    const result = await supertest(web).
+        get('/api/contacts').query({page: 2}).set('Authorization', 'test');
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(5);
+    expect(result.body.paging.page).toBe(2);
+    expect(result.body.paging.total_page).toBe(2);
+    expect(result.body.paging.total_item).toBe(15);
+  });
+
+  it('should search using name', async () => {
+    const result = await supertest(web).
+        get('/api/contacts').query({name: "test 1"}).set('Authorization', 'test');
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(6);
+    expect(result.body.paging.page).toBe(1);
+    expect(result.body.paging.total_page).toBe(1);
+    expect(result.body.paging.total_item).toBe(6);
+  });
+
+  it('should search using email', async () => {
+    const result = await supertest(web).
+        get('/api/contacts').query({email: "test1"}).set('Authorization', 'test');
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(6);
+    expect(result.body.paging.page).toBe(1);
+    expect(result.body.paging.total_page).toBe(1);
+    expect(result.body.paging.total_item).toBe(6);
+  });
+
+  it('should search using phone', async () => {
+    const result = await supertest(web).
+        get('/api/contacts').query({phone: "08123456781"}).set('Authorization', 'test');
+
+    logger.info(result.body);
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.length).toBe(6);
+    expect(result.body.paging.page).toBe(1);
+    expect(result.body.paging.total_page).toBe(1);
+    expect(result.body.paging.total_item).toBe(6);
   });
 });
