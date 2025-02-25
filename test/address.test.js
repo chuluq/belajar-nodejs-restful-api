e1d057d503debd1e5c96ba8b97a6d2b2dda81039
chuluq <chuluq@arvis.id>
2025-02-25 08:47:38 +0700

    feat: add update address api

--- test/address.test.js
+++ test/address.test.js
@@ -131,3 +131,97 @@ describe('GET /api/contacts/:contactId/addresses/:addressId', () => {
     expect(result.status).toBe(404);
   });
 });
+
+describe('PUT /api/contacts/:contactId/addresses/:addressId', () => {
+  beforeEach(async () => {
+    await createTestUser();
+    await createTestContact();
+    await createTestAddress();
+  });
+
+  afterEach(async () => {
+    await removeAllTestAddresses();
+    await removeAllTestContacts();
+    await removeTestUser();
+  });
+
+  it('should update existing address', async () => {
+    const testContact = await getTestContact();
+    const testAddress = await getTestAddress();
+
+    const result = await supertest(web).
+        put('/api/contacts/' + testContact.id + '/addresses/' + testAddress.id).
+        set('Authorization', 'test').
+        send({
+          street: 'Street',
+          city: 'City',
+          province: 'Province',
+          country: 'Indonesia',
+          postal_code: '1111',
+        });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.id).toBe(testAddress.id);
+    expect(result.body.data.street).toBe('Street');
+    expect(result.body.data.city).toBe('City');
+    expect(result.body.data.province).toBe('Province');
+    expect(result.body.data.country).toBe('Indonesia');
+    expect(result.body.data.postal_code).toBe('1111');
+  });
+
+  it('should reject if request is not valid', async () => {
+    const testContact = await getTestContact();
+    const testAddress = await getTestAddress();
+
+    const result = await supertest(web).
+        put('/api/contacts/' + testContact.id + '/addresses/' + testAddress.id).
+        set('Authorization', 'test').
+        send({
+          street: 'Street',
+          city: 'City',
+          province: 'Province',
+          country: '',
+          postal_code: '',
+        });
+
+    expect(result.status).toBe(400);
+  });
+
+  it('should reject if address is not found', async () => {
+    const testContact = await getTestContact();
+    const testAddress = await getTestAddress();
+
+    const result = await supertest(web).
+        put('/api/contacts/' + testContact.id + '/addresses/' +
+            (testAddress.id + 1)).
+        set('Authorization', 'test').
+        send({
+          street: 'Street',
+          city: 'City',
+          province: 'Province',
+          country: 'Indonesia',
+          postal_code: '1111',
+        });
+
+    expect(result.status).toBe(404);
+  });
+
+  it('should reject if contact is not found', async () => {
+    const testContact = await getTestContact();
+    const testAddress = await getTestAddress();
+
+    const result = await supertest(web).
+        put('/api/contacts/' + (testContact.id + 1) + '/addresses/' +
+            (testAddress.id)).
+        set('Authorization', 'test').
+        send({
+          street: 'Street',
+          city: 'City',
+          province: 'Province',
+          country: 'Indonesia',
+          postal_code: '1111',
+        });
+
+    expect(result.status).toBe(404);
+  });
+});
